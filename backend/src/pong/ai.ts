a53490fc6d0b7a4b5e349b9cb8ef6718971d6f56
anclarma <anclarma@student.42.fr>
2022-09-05 17:33:10 +0200

    maj ia

--- backend/src/pong/ai.ts
+++ backend/src/pong/ai.ts
@@ -1,3 +1,9 @@
+import { Line, Point } from 'geometric';
+import {
+  checkIntersection,
+  colinearPointWithinSegment,
+  IntersectionCheckResult,
+} from 'line-intersect';
 import { IGameStates } from './interfaces/game-states.interface';
 import { IGame } from './interfaces/game.interface';
 import { IInput } from './interfaces/input.interface';
@@ -80,17 +86,86 @@ export class Ai {
         racket.height,
       );
     } else {
+      const ball: Point = [
+        this.game.states.ball.left + this.game.ball.diammeter / 2,
+        this.game.states.ball.top + this.game.ball.diammeter / 2,
+      ];
       return this.ketToCenter(
-        this.predictCenter(racket),
+        this.predictCenter(racket, ball, [
+          this.game.states.ballDirection[0],
+          this.game.states.ballDirection[1],
+        ]),
         racket.top + racket.height / 2,
         racket.height,
       );
     }
   }
 
-  private predictCenter(racket: IRacket): number {
+  private predictCenter(
+    racket: IRacket,
+    ball: Point,
+    ballDirection: Point,
+  ): number {
+    //fonction recursive qui prend une nouvel ball et un nouveau vecteur vitesse a chaque iter
+
+    //nota: je considere une ball comme son centre:
+    // piste d'amelioration pour la colision avec les murs:
+    //   si la balle vas vers le haut je check top.ball
+    //   sinon top.ball + ball.diammeter
+
     // simuler toutes les collision avec les murs jusqu'a la racket
-    return 0;
+    //  line de position de ma ball et pos suivante
+    //  checker l'inter line/milieu.left de la raquette
+    //  si l'inter sort du jeu on applique un mur puis on recommence
+    //  sinon on renvoi le point d'inter
+
+    const racketCenter: Point = [
+      racket.left + racket.width / 2,
+      racket.top + racket.height / 2,
+    ];
+    const racketColision: IntersectionCheckResult = checkIntersection(
+      ball[0],
+      ball[1],
+      ball[0] + ballDirection[0],
+      ball[1] + ballDirection[1],
+      racketCenter[0],
+      0,
+      racketCenter[0],
+      this.game.board.height,
+    );
+    const wall: Point =
+      ballDirection[1] < 0 ? [0, 0] : [0, this.game.board.height];
+    const wallColision: IntersectionCheckResult = checkIntersection(
+      ball[0],
+      ball[1],
+      ball[0] + ballDirection[0],
+      ball[1] + ballDirection[1],
+      wall[0],
+      wall[1],
+      this.game.board.width,
+      wall[1],
+    );
+    if (
+      racketColision.type === 'intersecting' &&
+      colinearPointWithinSegment(
+        racketColision.point.x,
+        racketColision.point.y,
+        racketCenter[0],
+        0,
+        racketCenter[0],
+        this.game.board.height,
+      )
+    ) {
+      return racketColision.point.y;
+    } else if (wallColision.type === 'intersecting') {
+      return this.predictCenter(
+        racket,
+        [wallColision.point.x, wallColision.point.y],
+        [ballDirection[0], ballDirection[1] * -1],
+      );
+    } else {
+      return this.game.board.height / 2;
+    }
   }
 
   private ketToCenter(
